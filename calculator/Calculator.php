9b7d95da1b10a1a978212a97038230940006673b
Kovács Zoltán <zoltan.smith@gmail.com>
2015-04-15 20:46:34 +0200

    Check input strings

--- calculator/Calculator.php
+++ calculator/Calculator.php
@@ -1,5 +1,49 @@
 <?php namespace Calculator;
 
-class Calculator {
+class Calculator
+{
+    private $_error = false;
+    private $_data;
+    private $_availableOperators = ['+', '-', '*', '/'];
+
+    public function calculate($string)
+    {
+        $this->_error = false;
+        $this->_data  = explode(' ', $string);
+        $this->_checkString()->_checkDataStructure();
+    }
+
+    public function hasError()
+    {
+        return $this->_error;
+    }
+
+    private function _checkString()
+    {
+        if (count($this->_data) < 3)
+        {
+            $this->_error = true;
+        }
+
+        return $this;
+    }
+
+    private function _checkDataStructure()
+    {
+        if ($this->_error)
+        {
+            return false;
+        }
+
+        foreach ($this->_data as $index => $value)
+        {
+            if (($index % 2 == 0 && !preg_match('/^[0-9]+$/', $value))
+                || ($index % 2 == 1 && !in_array($value, $this->_availableOperators))
+            )
+            {
+                $this->_error = true;
+            }
+        }
+    }
 
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -13,9 +13,38 @@ class CalculatorTest extends PHPUnit_Framework_TestCase
         $this->_calculator = new Calculator();
     }
 
-    public function testNothing()
+    /**
+     * @dataProvider shortStringDataProvider
+     */
+    public function testTheStringMinimalLength($string)
     {
+        $this->_calculator->calculate($string);
+        $this->assertTrue($this->_calculator->hasError());
+    }
 
+    /**
+     * @dataProvider stringDataProvider
+     */
+    public function testDataArrayIsValidation($expected, $string)
+    {
+        $this->_calculator->calculate($string);
+        $this->assertSame($expected, $this->_calculator->hasError());
+    }
+
+    public function shortStringDataProvider()
+    {
+        return [ [''], ['1'], ['+1'], ['+1+'], ['+1 +'], ['1 +1']];
+    }
+
+    public function stringDataProvider()
+    {
+        return [
+            [false, '1 + 1'],
+            [false, '1 + 1 + 6 * 2'],
+            [true, '1 + 1+6 * 2'],
+            [true, '1+1+6*4'],
+            [false, '1 + 13'],
+        ];
     }
 
 }
